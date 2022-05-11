f30e978468112079ab847a6493d188b82bd654d1
Wajeah Raja <rajawajeah@gmail.com>
2022-05-10 23:36:59 -0400

    moving portfolio thumbnail a little bit higher adding links to nav console

--- src/Components/NavConsole.js
+++ src/Components/NavConsole.js
@@ -4,6 +4,11 @@ const NavConsole = ()=> {
         <div className="console">
             <h3 className='name-header'>Wajeah Raja</h3>
             <img className='profile-thumbnail' src='https://i.imgur.com/jyPZwXI.png'  />
+            <ul>
+                <li><a></a></li>
+                <li><a></a></li>
+                <li><a></a></li>
+            </ul>
         </div>
     )
 }
