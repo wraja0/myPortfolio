5aba1091f1fccaf1bdc7b326bac85a14aa4e0006
Wajeah Raja <rajawajeah@gmail.com>
2022-05-19 23:56:28 -0400

    changing backround from an image to a css background

---
+++ src/Components/Tile1.js
@@ -0,0 +1,49 @@
+import '../styles/AppBody.css'
+import { useState, useEffect, useRef } from 'react'
+const Tile1 = ()=> {
+    return (
+        <div>
+            <h1 className='home-title'>I'm a</h1>
+            <div className='container'> 
+              <div className='home-title typed-out'>
+                Web Developer
+              </div>
+            </div>
+            <br />
+            <div className='container'> 
+              <div className='home-title typed-out'>
+                Web Developer
+              </div>
+            </div>
+            <br />
+            <div className='container'> 
+              <div className='home-title typed-out'>
+                Web Developer
+              </div>
+            </div>
+            <div className='pre-content'>
+              <div className='pre-projects'>
+              </div>
+              <div className='pre-exp'>
+              </div>
+            </div>
+            <div className='skill-thumbnails'>
+              <div className='react-thumbnail'>
+              </div>
+              <div className='node-thumbnail'>
+                </div>
+                <div className='postgreSQL-thumbnail'>
+                </div>
+            </div>
+            <div id="container">
+              <button className="learn-more">
+                <span className="circle" aria-hidden="true">
+                  <span className="icon arrow"></span>
+                </span>
+                <span className="button-text">Contact Me</span>
+              </button>
+            </div> 
+        </div>
+    )
+}
+export default Tile1
